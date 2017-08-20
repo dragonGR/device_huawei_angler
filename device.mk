1cb510081b68be784b1132faa6cfa69fa5f519dc
razorloves <razorloves@gmail.com>
2017-08-20 13:59:38 +0300

    angler: don't inherit wifi firmware
    The wifi firmware being used is on /vendor.
    This was just copying old firmware to /system.
    Changed to just inherit the config files.
    
    ** also remove whitespace on Line 518.
    
    Change-Id: Id28c81a3688dfad13943b0c47ed70c8f84f14e1d

--- device.mk
+++ device.mk
@@ -515,13 +515,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # miracast props
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.debug.wfd.enable=1﻿
+    persist.debug.wfd.enable=1
 
 $(call inherit-product-if-exists, hardware/qcom/msm8994/msm8994.mk)
 $(call inherit-product-if-exists, vendor/qcom/gpu/msm8994/msm8994-gpu-vendor.mk)
 
-# copy wlan firmware
-$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4358/device-bcm.mk)
+# copy wlan configs
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 
 # GPS configuration
 PRODUCT_COPY_FILES += \
