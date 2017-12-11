8540280243e0ec720ce6ac007cbd1cfd26a1e83a
doc HD <doc.divxm@gmail.com>
2017-12-11 22:28:49 +0300

    angler: AICPfy
    Change-Id: I5d9783d01db4f37340b110f53faff0e7a4463db0

--- aicp.mk
+++ aicp.mk
@@ -3,17 +3,21 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/angler/aosp_angler.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_angler
+PRODUCT_NAME := aicp_angler
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6P
 
 TARGET_VENDOR := huawei
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Davor B (LorD ClockaN), doc HD (semdoc)"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
