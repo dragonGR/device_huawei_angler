16f73cf0b78f0ebc49c8583ed5a1b03a8106a3b5
razorloves <razorloves@gmail.com>
2018-04-04 23:06:00 +0300

    angler: Fix missing space
    Change-Id: Id055656ad95717094546b54e427fa5298f10eeb2

--- aicp.mk
+++ aicp.mk
@@ -23,4 +23,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
     PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM3.171019.019 4618994 release-keys"
 
-BUILD_FINGERPRINT :=google/angler/angler:8.1.0/OPM3.171019.019/4618994:user/release-keys
+BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM3.171019.019/4618994:user/release-keys
